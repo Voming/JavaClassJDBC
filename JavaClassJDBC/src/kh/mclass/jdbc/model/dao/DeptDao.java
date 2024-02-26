f08ef9af0d02f2639d3e07b93477ac037ac62da2
Voming <ebomin1234@gmail.com>
2024-02-26 14:47:29 +0900

    DEPT완료

--- JavaClassJDBC/src/kh/mclass/jdbc/model/dao/DeptDao.java
+++ JavaClassJDBC/src/kh/mclass/jdbc/model/dao/DeptDao.java
@@ -2,12 +2,6 @@ package kh.mclass.jdbc.model.dao;
 
 //import static kh.mclass.jdbc.common.JdbcTemplate.*;
 import static kh.mclass.jdbc.common.JdbcTemplate.close;
-import static kh.mclass.jdbc.common.JdbcTemplate.getConnection;
-//static과 .*을 적으면 해당 클래스의 멤버 변수, 메소드를 의미함
-//단, static일 경우만 가능함
-//extends 에도 찾을 수 없고 기본 제공 메소드가 아니라면 import static을 찾아야함
-//기울어진 메소드
-//마치 extends한 효과
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -29,7 +23,6 @@ public class DeptDao {
 		ResultSet rs = null;
 		List<Dept> result = null;
 		try {
-			conn = getConnection(); // 연결 부분을 메소드 호출로 간단하게 구현
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 			result = new ArrayList<Dept>();
@@ -58,8 +51,6 @@ public class DeptDao {
 		PreparedStatement pstmt = null;
 		int result = 0;
 		try {
-			conn = getConnection(); // 연결 부분을 메소드 호출로 간단하게 구현
-
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, dept.getDeptno());
 			pstmt.setString(2, dept.getDname());
@@ -81,9 +72,7 @@ public class DeptDao {
 		int result = 0;
 
 		try {
-			conn = getConnection();
 			pstmt = conn.prepareStatement(sql);
-
 			pstmt.setString(1, dname);
 
 			result = pstmt.executeUpdate();
